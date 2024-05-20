a5e98fb9e95ffc638faebe9056e2069d6f5b6aed
octav19 <octavian.susanu@epfl.ch>
2024-05-20 02:10:17 +0200

    First test

--- tests/test_C_S_bond.py
+++ tests/test_C_S_bond.py
@@ -1,1 +1,8 @@
 from chemicalbd.bond_disconnector import C_S_disconnection
+from rdkit import Chem
+
+def C_S_disconnection_absent():
+    mol = Chem.MolFromSmiles('CCC')
+    assert C_S_disconnection(mol) == 0
+
+
