9e96468b04d8cd007f2ab0336120b8772f63a5ed
octav19 <octavian.susanu@epfl.ch>
2024-05-20 17:50:48 +0200

    Hope it works

--- tests/test_C_S_disconnection.py
+++ tests/test_C_S_disconnection.py
@@ -1,7 +1,14 @@
 from chemicalbd.bond_disconnector import C_S_disconnection
 from rdkit import Chem
+import pytest
 
 def test_C_S_absent():
-    mol = Chem.MolFromSmiles('CSCC')
+    mol = Chem.MolFromSmiles('CCC')
     assert C_S_disconnection(mol) == 0
+
+def test_C_S_invalid ():
+    mol = 'Ups'
+    with pytest.raises(TypeError):
+        C_S_disconnection(mol)
+    
 
