30125bfc5e6d64a320f3cc322ca664a12cc5e318
octav19 <octavian.susanu@epfl.ch>
2024-05-20 02:31:43 +0200

    Maybe

--- tests/test_C_S_bond.py
+++
@@ -1,8 +0,0 @@
-from bond_disconnector import C_S_disconnection
-from rdkit import Chem
-
-def C_S_disconnection_absent():
-    mol = Chem.MolFromSmiles('CCC')
-    assert C_S_disconnection(mol) == 0
-
-
