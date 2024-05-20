fdf8d29ebac6f8d0d99703c4039b9a04fa40aff4
octav19 <octavian.susanu@epfl.ch>
2024-05-20 17:46:08 +0200

    Works once again

--- tests/test_C_S_disconnection.py
+++ tests/test_C_S_disconnection.py
@@ -2,6 +2,6 @@ from chemicalbd.bond_disconnector import C_S_disconnection
 from rdkit import Chem
 
 def test_C_S_absent():
-    mol = Chem.MolFromSmiles('CCC')
+    mol = Chem.MolFromSmiles('CSCC')
     assert C_S_disconnection(mol) == 0
 
